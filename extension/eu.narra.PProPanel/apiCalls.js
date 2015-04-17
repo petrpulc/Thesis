b9f14ac68eed1893ba19e10bbcc2bb2c66101f89
akasummer <vanyaginds@gmail.com>
2015-04-17 11:51:12 +0200

    refactoring

--- extension/eu.narra.PProPanel/apiCalls.js
+++ extension/eu.narra.PProPanel/apiCalls.js
@@ -1,22 +1,17 @@
 var token;
 
 function getToken() {
-  token = $("#token").val();
+  localStorage["token"] = $("#token").val();
 }
 
 function loadProfile() {
   $.ajax({
     method: "GET",
-    url: "http://api.narra.eu/v1/users/me?token=" + token,
+    url: "http://api.narra.eu/v1/users/me?token=" + localStorage["token"],
     dataType: "json",
-
-    beforeSend: function() {
-      updateStatus("Loading profile...", 0);
-    },
 
     success: function(msg) {
       displayUsername(msg);
-      updateStatus("Loading projects...", 20);
     },
     error: function() {
       displayError("Cannot load profile.");
@@ -25,57 +20,44 @@ function loadProfile() {
 }
 
 function loadProjects() {
-  if (projectsJsonObject) {
-    displayProjects(projectsJsonObject);
-    hideProgressBar();
-  }
-  else {
-    $.ajax({
-      method: "GET",
-      url: "http://api.narra.eu/v1/projects?token=" + token,
-      dataType: "json",
+  $.ajax({
+    method: "GET",
+    url: "http://api.narra.eu/v1/projects?token=" + localStorage["token"],
+    dataType: "json",
 
-      success: function(msg) {
-        projectsJsonObject = msg;
-        displayProjects(msg);
-        hideProgressBar();
-      },
-      error: function() {
-        displayError("Cannot load projects.");
-      }
-    });
-  }
+    success: function(msg) {
+      displayProjects(msg);
+    },
+    error: function() {
+      displayError("Cannot load projects.");
+    }
+  });
 }
 
 function loadProjectLibraries(name) {
-  if (librariesJsonObject[name])
-    displayProjectLibraries(librariesJsonObject[name]);
-  else {
-    $.ajax({
-      method: "GET",
-      url: "http://api.narra.eu/v1/projects/" + name + "?token=" + token,
-      dataType: "json",
+  $.ajax({
+    method: "GET",
+    url: "http://api.narra.eu/v1/projects/" + name + "?token=" + localStorage["token"],
+    dataType: "json",
 
-      success: function(msg) {
-        librariesJsonObject[name] = msg;
-        displayProjectLibraries(msg);
-      },
-      error: function() {
-        displayError("Cannot load libraries.");
-      }
-    });
-  }
+    success: function(msg) {
+      displayProjectLibraries(msg);
+    },
+    error: function() {
+      displayError("Cannot load libraries.");
+    }
+  });
 }
 
 function logout() {
   $.ajax({
     method: "GET",
-    url: "http://api.narra.eu/v1/users/me/signout?token=" + token,
+    url: "http://api.narra.eu/v1/users/me/signout?token=" + localStorage["token"],
     dataType: "json",
 
     success: function(msg) {
-      clearWrapper();
-      initPlugin();
+      localStorage.removeItem("token");
+      window.location.href = "index.html";
     },
     error: function() {
       displayError("Cannot signout.");
