ad6814fc2eff66bdf26f1de6e09fddd7abb35d72
akasummer <vanyaginds@gmail.com>
2015-04-28 20:15:57 +0200

    Almost finished

--- extension/eu.narra.PProPanel/apiCalls.js
+++ extension/eu.narra.PProPanel/apiCalls.js
@@ -1,5 +1,6 @@
 var token;
-var url = "http://neptun.avc-cvut.cz:8080/";
+var apiUrl = "http://neptun.avc-cvut.cz:8080/";
+var uiUrl = "http://neptun.avc-cvut.cz/";
 
 function getToken() {
   localStorage["token"] = $("#token input").val();
@@ -8,7 +9,7 @@ function getToken() {
 function loadProfile() {
   $.ajax({
     method: "GET",
-    url: url + "v1/users/me?token=" + localStorage["token"],
+    url: apiUrl + "v1/users/me?token=" + localStorage["token"],
     dataType: "json",
 
     success: function(msg) {
@@ -24,7 +25,7 @@ function loadProfile() {
 function loadProjects() {
   $.ajax({
     method: "GET",
-    url: url + "v1/projects?token=" + localStorage["token"],
+    url: apiUrl + "v1/projects?token=" + localStorage["token"],
     dataType: "json",
 
     success: function(msg) {
@@ -36,48 +37,37 @@ function loadProjects() {
   });
 }
 
-function loadProjectLibraries(name) {
-  if (!name) {
-    $.ajax({
-      method: "GET",
-      url: url + "v1/libraries/" + "?token=" + localStorage["token"],
-      dataType: "json",
+function loadLibraries(name) {
+  if (!name)
+    var urlPart = "v1/libraries/";
+  else
+    var urlPart = "v1/projects/" + name;
+  
+  $.ajax({
+    method: "GET",
+    url: apiUrl + urlPart + "?token=" + localStorage["token"],
+    dataType: "json",
 
-      success: function(msg) {
-        displayLibraries(msg);
-      },
-      error: function() {
-        displayError("Cannot load libraries.");
-      }
-    });
-  }
-  else {
-    $.ajax({
-      method: "GET",
-      url: url + "v1/projects/" + name + "?token=" + localStorage["token"],
-      dataType: "json",
-
-      success: function(msg) {
-        displayProjectLibraries(msg);
-      },
-      error: function() {
-        displayError("Cannot load libraries.");
-      }
-    });
-  }
+    success: function(msg) {
+      displayLibraries(msg);
+    },
+    error: function() {
+      displayError("Cannot load libraries.");
+    }
+  });
 }
 
 function loadLibraryItems(name) {
   $.ajax({
       method: "GET",
-      url: url + "v1/libraries/" + name + "/items?token=" + localStorage["token"],
+      url: apiUrl + "v1/libraries/" + name + "/items?token=" + localStorage["token"],
       dataType: "json",
 
       success: function(msg) {
         displayLibraryItems(msg);
       },
       error: function() {
-        displayError("Cannot load libraries.");
+        displayError("Cannot load items.");
       }
     });
 }
@@ -85,7 +75,7 @@ function loadLibraryItems(name) {
 function logout() {
   $.ajax({
     method: "GET",
-    url: url + "v1/users/me/signout?token=" + localStorage["token"],
+    url: apiUrl + "v1/users/me/signout?token=" + localStorage["token"],
     dataType: "json",
 
     success: function(msg) {
@@ -102,9 +92,10 @@ function callbackFunction(data) {
   var array = data.split(',');
   var counter = 0;
   for (var i = 0; i < array.length; i++) {
+    array[i] = array[i].substring(array[i].lastIndexOf('\\') + 1);
     $.ajax({
       method: "GET",
-      url: url + "v1/items/" + array[i].replace(/\.[^/.]+$/, "") + "?token=" + localStorage["token"],
+      url: apiUrl + "v1/items/" + array[i].replace(/\.[^/.]+$/, "") + "?token=" + localStorage["token"],
       dataType: "json",
 
       success: function(msg) {
@@ -115,6 +106,7 @@ function callbackFunction(data) {
           $("#sync i").removeClass("fa-spin"); 
       },
       error: function() {
+        displayError("Cannot sync items.");
         counter++;
         if (counter == array.length)
           $("#sync i").removeClass("fa-spin");
--- extension/eu.narra.PProPanel/ui.js
+++ extension/eu.narra.PProPanel/ui.js
@@ -1,5 +1,10 @@
 function displayError(text) {
+	$("#status").html(text);
+	setTimeout(clearStatus,5000);
+}
 
+function clearStatus() {
+	$("#status").html("");
 }
 
 function displayUsername(userObject) {
@@ -24,38 +29,13 @@ function displayProjects(projectsObject) {
   });
 }
 
-function displayProjectLibraries(librariesObject) {
-	var libraries = '';
-	$("#ui_content h1").html('Project: ' + librariesObject.project.title);
-	for (var i = librariesObject.project.libraries.length - 1; i >= 0; i--) {
-		libraries = libraries + '<li class="project" id="' + librariesObject.project.libraries[i].id + '"><div><input type="checkbox" name="libraries" value="' + librariesObject.project.libraries[i].id + '"/><ul class="thumbnails">';
-		if (librariesObject.project.libraries[i].thumbnails) {
-			for (var j = librariesObject.project.libraries[i].thumbnails.length - 1; j >= 0; j--) {
-				libraries = libraries + '<li style="background-image: url(\'' + librariesObject.project.libraries[i].thumbnails[j] +'\');"></li>'
-			}
-		}
-		libraries = libraries + '</ul><div class="project_title"><span class="default">' + librariesObject.project.libraries[i].name + '</span></div></div></li>';
-	}
-	$("#ui_content_projects").append(libraries);
-	$(".thumbnails").on("click", function(e) {
-    var name = $(this).parent().parent().attr('id');
-    window.location.href = "items.html?name=" + name;
-  });
-	$(".project input").change(
-    function(){
-        if ($(this).is(':checked')) {
-            $("#elemnum").html(function(i, val) { return +val+1 });
-        }
-        else {
-        	$("#elemnum").html(function(i, val) { return +val-1 });
-        }
-    });
-}
-
 function displayLibraries(librariesObject) {
+	if (librariesObject.project)
+		librariesObject = librariesObject.project;
+
 	var libraries = '';
 	for (var i = librariesObject.libraries.length - 1; i >= 0; i--) {
-		libraries = libraries + '<li class="project" id="' + librariesObject.libraries[i].id + '"><div><input type="checkbox" name="libraries" value="' + librariesObject.libraries[i].id + '"/><ul class="thumbnails">';
+		libraries = libraries + '<li class="project" id="' + librariesObject.libraries[i].id + '"><div><ul class="thumbnails">';
 		if (librariesObject.libraries[i].thumbnails) {	
 			for (var j = librariesObject.libraries[i].thumbnails.length - 1; j >= 0; j--) {
 				libraries = libraries + '<li style="background-image: url(\'' + librariesObject.libraries[i].thumbnails[j] +'\');"></li>'
@@ -64,43 +44,28 @@ function displayLibraries(librariesObject) {
 		libraries = libraries + '</ul><div class="project_title"><span class="default">' + librariesObject.libraries[i].name + '</span></div></div></li>';
 	}
 	$("#ui_content_projects").append(libraries);
+
 	$(".thumbnails").on("click", function(e) {
     var name = $(this).parent().parent().attr('id');
     window.location.href = "items.html?name=" + name;
   });
-	$(".project input").change(
-    function(){
-      if ($(this).is(':checked')) {
-        $("#elemnum").html(function(i, val) { return +val+1 });
-      }
-      else {
-      	$("#elemnum").html(function(i, val) { return +val-1 });
-      }
-    });
 }
 
 function displayLibraryItems(itemsObject) {
 	var items = '';
 	for (var i = itemsObject.items.length - 1; i >= 0; i--) {
-		items = items + '<li class="item" id="' + itemsObject.items[i].id + '"><div class="item_wrapper"><input type="checkbox" name="items" value="' + itemsObject.items[i].id + '"/><ul class="thumbnails">';
+		items = items + '<li class="item" id="' + itemsObject.items[i].id + '"><div class="item_wrapper"><ul class="thumbnails">';
 		items = items + '<li style="background-image: url(\'' + itemsObject.items[i].thumbnails[0] +'\');"></li>'
 		items = items + '</ul><div class="item_title"><span class="default">' + itemsObject.items[i].name + '</span></div></div></li>';
 	}
 	$("#ui_content_projects").append(items);
+
 	$(".thumbnails").on("dblclick", function(e) {
-    var name = $(this).parent().parent().attr('id');
-    window.location.href = "items.html?name=" + name;
+    var id = $(this).parent().parent().attr('id');
+
+    var csInterface = new CSInterface();
+    csInterface.openURLInDefaultBrowser(uiUrl + "items/" + id + "?token=" + localStorage["token"]);
   });
-	$(".item input").change(
-    function() {
-      if ($(this).is(':checked')) {
-        $("#elemnum").html(function(i, val) { return +val+1 });
-      }
-      else {
-      	$("#elemnum").html(function(i, val) { return +val-1 });
-      }
-  	}
-  );
 }
 
 
